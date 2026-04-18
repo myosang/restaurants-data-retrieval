c6289aa304d5417348b14c97bc5c52092cb4cef0
Hyosang Kim <gytkd0303@gmail.com>
2026-04-18 15:29:43 +0200

    feat: Add API get requests and headers

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+# headers used in the Take it away UK restaurants endpoint
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.just-eat.co.uk/"
+}
+
+def get_data(endpoint_url: str, headers: dict):
+    response = requests.get(url=endpoint_url, headers=headers)
+    return response.json()
